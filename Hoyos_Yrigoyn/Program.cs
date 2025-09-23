d239506c61758d222ead7fc96d60385cd546f4c6
Milagro Asencio <N00466501@upn.pe>
2025-09-23 16:50:22 -0500

    Primer problema

--- Hoyos_Yrigoyn/Program.cs
+++ Hoyos_Yrigoyn/Program.cs
@@ -10,6 +10,26 @@ namespace Hoyos_Yrigoyn
     {
         static void Main(string[] args)
         {
+            // Bienvenido a entrada de datos
+
+            Console.WriteLine("---CALCULADORA INTELIGENTE DE PROMEDIOS---");
+
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+
+            float nota1, nota2, nota3, promedio;
+
+            Console.WriteLine("Ingrese la primera nota:");
+            nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda nota:");
+            nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercera nota:");
+            nota3 = float.Parse(Console.ReadLine());
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine("El promedio de " + nombre + " es: " + promedio);
+
+            Console.ReadKey();
         }
     }
 }
