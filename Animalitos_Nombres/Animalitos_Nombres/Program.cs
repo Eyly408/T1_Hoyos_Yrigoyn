a181319a2c758ae4807427a9dfcb964f029a728f
Eyly408 <n00478352@upn.pe>
2025-11-12 15:15:21 -0500

    Add files via upload
    Subir archivos

---
+++ Animalitos_Nombres/Animalitos_Nombres/Program.cs
@@ -0,0 +1,98 @@
+﻿using Bilbioteca_oficial_animalitos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animalitos_Nombres
+{
+
+        class Program
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                Mascotas misMascotas = new Mascotas();
+
+                int opcion;
+
+
+
+                do
+
+                {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("_________MENÚ DE MASCOTAS_______");
+                Console.ResetColor();
+
+                    Console.WriteLine("[1] Insertar mascota");
+
+                    Console.WriteLine("[2] Mostrar mascotas");
+
+                    Console.WriteLine("[3] Ordenar (burbuja)");
+
+                    Console.WriteLine("[4] Salir");
+
+                 Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Elija una opción: ");
+                Console.ResetColor();
+
+
+                    int.TryParse(Console.ReadLine(), out opcion);
+
+                    Console.WriteLine();
+                Console.Clear();
+              
+                    switch (opcion)
+
+                    {
+
+                        case 1:
+
+                            misMascotas.InsertarMascota();
+
+                            break;
+
+                        case 2:
+
+                            misMascotas.MostrarMascotas();
+
+                            break;
+
+                        case 3:
+
+                            misMascotas.OrdenarMascotas();
+
+                            break;
+
+                        case 4:
+
+                            Console.WriteLine("Saliendo...");
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("Opción no válida.");
+
+                            break;
+
+                    }
+
+
+
+
+
+                } while (opcion != 0);
+
+            }
+
+        }
+
+    }
+    
+
---
+++ Biblioteca_oficial_animalitos/Biblioteca_oficial_animalitos/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilbioteca_oficial_animalitos
+{
+
+    public class Mascotas
+
+    {
+
+        private List<string> listaMascotas = new List<string>();
+
+
+
+        public void InsertarMascota()
+
+        {
+            Console.WriteLine("Ingrese la cantidad de mascotas que va a ingresar");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+              
+
+            for (int i = 0; i< cantidad; i++)
+            {
+                Console.Write("Ingrese el nombre de la mascota: ");
+
+                string nombre = Console.ReadLine();
+
+                listaMascotas.Add(nombre);
+            }
+
+
+            
+
+            Console.WriteLine("Mascota agregada correctamente.");
+
+        }
+
+
+        public void MostrarMascotas()
+
+        {
+
+            if (listaMascotas.Count == 0)
+
+            {
+
+                Console.WriteLine("No hay mascotas registradas.");
+
+                return;
+
+            }
+
+
+
+            Console.WriteLine("Lista de mascotas:");
+
+            for (int i = 0; i < listaMascotas.Count; i++)
+
+            {
+
+                Console.WriteLine("- " + listaMascotas[i]);
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public void OrdenarMascotas()
+
+        {
+
+            int n = listaMascotas.Count;
+
+            if (n <= 1)
+
+            {
+
+                Console.WriteLine("No hay suficientes mascotas para ordenar");
+
+                return;
+
+            }
+
+
+
+            for (int i = 0; i < n - 1; i++)
+
+            {
+
+                for (int j = 0; j < n - i - 1; j++)
+
+                {
+
+                    if (string.Compare(listaMascotas[j], listaMascotas[j + 1]) > 0)
+
+                    {
+
+                        string temp = listaMascotas[j];
+
+                        listaMascotas[j] = listaMascotas[j + 1];
+
+                        listaMascotas[j + 1] = temp;
+
+                    }
+
+                }
+
+            }
+
+
+
+            Console.WriteLine("Lista de mascotas ordenada correctamente.");
+        }
+    }
+}
+
+
+
+
